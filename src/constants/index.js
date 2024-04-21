fe7921ac7c2fcd083925b318e13a712bd146be43
Bikki <bikrambhusal75@gmail.com>
2024-04-21 09:27:48 +0545

    logo_changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
+// import project3 from "../assets/projects/project-3.jpg";
 
 export const HERO_CONTENT = `I am an aspiring web developer with a passion for crafting robust and scalable web applications.I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
